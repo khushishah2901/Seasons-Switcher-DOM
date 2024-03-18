a8b190a593c761c62e3a848eb2996930b4d5f8c4
khushishah2901 <121679622+khushishah2901@users.noreply.github.com>
2024-03-18 08:20:32 +0530

    Seasons changer

---
+++ index.js
@@ -0,0 +1,23 @@
+let backgroundImageEl = document.getElementById("backgroundImage");
+
+function khushi() {
+    backgroundImageEl.style.backgroundImage = 'url("spring sesion.jpg")';
+    
+}
+
+function dusarabtn() {
+    backgroundImageEl.style.backgroundImage = 'url("summer.jpg")';
+    
+
+}
+function tisarabtn(){
+    backgroundImageEl.style.backgroundImage = 'url("autumna sesion.jpg")';
+
+}
+
+function chauthabtn(){
+    backgroundImageEl.style.backgroundImage = 'url("nature.jpg")';
+    
+
+}
+
